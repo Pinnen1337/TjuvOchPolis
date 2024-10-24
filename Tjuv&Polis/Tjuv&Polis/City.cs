5d2568775a75c30fe75a41d9329dcfe0f140af10
Fredrik von Post <fredrik.v.post@gmail.com>
2024-10-24 10:37:13 +0200

    City.cs Refactor draw methods, try to use SetCiursorPosition

--- Tjuv&Polis/Tjuv&Polis/City.cs
+++ Tjuv&Polis/Tjuv&Polis/City.cs
@@ -3,57 +3,54 @@ namespace Tjuv_Polis
 {
     public class City
     {
-        public int Roof { get; set; }
-        public int Wall { get; set; }
-        public int Space { get; set; }
-        public int Floor { get; set; }
+        public int HorisontalWallLength { get; set; }
+        public int VerticalWallLength { get; set; }
 
-        public City(int roof, int wall, int space, int floor)
+        public City(int horisontalSize, int verticalSize)
         {
-            Roof = roof;
-            Wall = wall;
-            Space = space;
-            Floor = floor;
+            HorisontalWallLength = horisontalSize;
+            VerticalWallLength = verticalSize;
         }
 
         public void DrawCity()
         {
-            DrawRoof();
+            DrawTopWall();
+            DrawLowerWall();
             DrawWalls();
-            DrawFloor();
         }
 
-        private void DrawRoof()
+        private void DrawTopWall()
         {
-            for (int i = 0; i < Roof; i++)
+            Console.SetCursorPosition(0, 0);
+            for (int i = 0; i < HorisontalWallLength; i++)
             {
                 Console.Write("=");
             }
-            Console.WriteLine();
+        }
+        private void DrawLowerWall()
+        {
+            Console.SetCursorPosition(0, 25);
+            for (int i = 0; i < HorisontalWallLength; i++)
+            {
+                Console.Write("="); //Golv
+            }
         }
 
         private void DrawWalls()
         {
-            for (int wall = 0; wall < 25; wall++)
+            int cursorPositionLeft = 0;
+            int cursorPositionTop = 1;
+            for (int wall = 0; wall < VerticalWallLength; wall++)
             {
+                Console.SetCursorPosition(cursorPositionLeft, cursorPositionTop); 
                 Console.Write("░");  //Vänster vägg
-
-                for (int space = 0; space < 98; space++)
-                {
-                    Console.Write(" "); //Mellanrum
-                }
+                Console.SetCursorPosition(cursorPositionLeft + HorisontalWallLength, cursorPositionTop);
                 Console.WriteLine("░"); //Höger vägg
+                cursorPositionTop++;
             }
         }
 
-        private void DrawFloor()
-        {
-            for (int floor = 0; floor < 100; floor++)
-            {
-                Console.Write("="); //Golv
-            }
-            Console.WriteLine();
-        }
+
 
     }
 
