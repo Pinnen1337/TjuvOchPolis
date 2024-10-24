c295141d3055c9bc85b14d23da149cc9000a4dd2
Christoffer Johansson <c.johansson90@live.se>
2024-10-24 08:57:41 +0200

    First commit

---
+++ Tjuv&Polis/Tjuv&Polis/Person.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Tjuv_Polis
+{
+    internal class Person
+    {
+        public int XPosition { get; set; }
+        public int YPosition { get; set; }
+        public char Symbol { get; set; }
+        public ConsoleColor Color { get; set; }
+
+        public Person()
+        {
+            Random random = new Random();
+            XPosition = random.Next(2, 99);
+            YPosition = random.Next(2, 24);
+        }
+    }
+
+    class Civilian : Person
+    {
+        public Civilian() : base()
+        {
+            Symbol = 'C';
+            Color = ConsoleColor.Green;
+        }
+    }
+
+    class Thief : Person
+    {
+        public Thief() : base()
+        {
+            Symbol = 'T';
+            Color = ConsoleColor.Red;
+        }
+    }
+
+    class Police : Person
+    {
+        public Police() : base()
+        {
+            Symbol = 'P';
+            Color = ConsoleColor.Blue;
+        }
+    }
+}
---
+++ Tjuv&Polis/Tjuv&Polis/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace Tjuv_Polis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            List<Person> persons = new List<Person>();
+            int numberOfEachType = 10;
+
+            for (int civilians = 0; civilians < numberOfEachType; civilians++)
+            {
+                persons.Add(new Civilian());
+            }
+
+            for (int thiefs = 0; thiefs < numberOfEachType; thiefs++)
+            {
+                persons.Add(new Thief());
+            }
+
+            for (int police = 0; police < numberOfEachType; police++)
+            {
+                persons.Add(new Police());
+            }
+
+            while(true)
+            {
+                Console.Clear();
+                RitaStaden();
+
+                foreach (Person person in persons)
+                {
+                    Console.SetCursorPosition(person.XPosition, person.YPosition); //Ska vi byta ut denna funktion?
+                    Console.ForegroundColor = person.Color;
+                    Console.Write(person.Symbol);
+                    Console.ResetColor();
+                }
+
+                //Thread.Sleep(500);
+                //Console.ReadKey();
+                Console.ReadLine();
+            }
+        }
+        static void RitaStaden()
+        {
+            for (int tak = 0; tak < 100; tak++)
+            {
+                Console.Write("=");
+
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < 25; i++)
+            {
+                Console.Write("░");  //Vänster vägg
+
+                for (int j = 0; j < 98; j++)
+                {
+                    Console.Write(" "); // Mellanrum
+                }
+                Console.WriteLine("░"); //Höger vägg
+            }
+
+
+            for (int golv = 0; golv < 100; golv++)
+            {
+                Console.Write("=");
+            }
+            Console.WriteLine();
+        }
+    }
+}
