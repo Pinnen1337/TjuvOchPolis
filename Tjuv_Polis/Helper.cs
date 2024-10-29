47708d8cc39b459c8e692b8b432ed1e25cd3c584
Fredrik von Post <fredrik.v.post@gmail.com>
2024-10-29 12:46:10 +0100

    First attempt at Lists in lists

--- Tjuv_Polis/Helper.cs
+++ Tjuv_Polis/Helper.cs
@@ -4,8 +4,8 @@ internal class Helper
 {
     public static bool CheckCollision(List<Person> persons)
     {
-        //List<Person> personsAtSameLocation = new List<Person>();
-        //List<List<Person>> collidingPersons = new List<perssons>();
+        List<Person> personsAtSameLocation = new List<Person>();
+        List<List<Person>> collidingPersons = new List<List<Person>>();
         foreach (Person thisPerson in persons)
         {
             foreach (Person otherPerson in persons)
@@ -14,11 +14,20 @@ internal class Helper
                 {
                     if (thisPerson.XPosition == otherPerson.XPosition && thisPerson.YPosition == otherPerson.YPosition)
                     {
-                        return true;
+                        if (personsAtSameLocation.Count == 0)
+                        {
+                            personsAtSameLocation.Add(thisPerson);
+                            personsAtSameLocation.Add(otherPerson);
+                        }
+                        else
+                        {
+                            personsAtSameLocation.Add(otherPerson);
+                        }
                     }
                 }
             }
+            collidingPersons.Add(personsAtSameLocation);
         }
-        return false;
+        return (collidingPersons.Count != 0 );
     }
 }
