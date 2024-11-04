93ac1ed59a65fe9d293e96e711202bd36a046a2b
Fredrik von Post <fredrik.v.post@gmail.com>
2024-11-04 15:23:12 +0100

    Add Message class to be able to insert ID to message instance

--- Tjuv_Polis/NewsFeed.cs
+++ Tjuv_Polis/NewsFeed.cs
@@ -3,19 +3,23 @@ namespace Tjuv_Polis;
 
 public class NewsFeed
 {
-    public Queue<string> NewsQueue { get; }
+    public Queue<Message> NewsQueue { get; }
     private readonly int _maxCount;
     private readonly int _startDrawAtX;
     private readonly int _startDrawAtY;
+    private int messageCounter = 1;
+
     public NewsFeed(int positionX, int positionY, int maxCount)
     {
-        NewsQueue = new Queue<string>();
+        NewsQueue = new Queue<Message>();
         _startDrawAtX = positionX;
         _startDrawAtY = positionY;
         _maxCount = maxCount;
     }
-    public void Add(string message)
+    public void Add(string text)
     {
+        Message message = new Message(messageCounter, text);
+        messageCounter++;
         NewsQueue.Enqueue(message);
         if (NewsQueue.Count > _maxCount)
         {
@@ -29,15 +33,27 @@ public class NewsFeed
         int rowOffset = 0;
         foreach (var message in NewsQueue)
         {
-            if (!string.IsNullOrWhiteSpace(message))
-            {
-                Console.SetCursorPosition(_startDrawAtX, _startDrawAtY + rowOffset);
-                Console.Write(new string(' ', 100));
-                Console.SetCursorPosition(_startDrawAtX, _startDrawAtY + rowOffset);
-                Console.Write(message);
-            }
+            Console.SetCursorPosition(_startDrawAtX, _startDrawAtY + rowOffset);
+            Console.Write(new string(' ', 100));
+            Console.SetCursorPosition(_startDrawAtX, _startDrawAtY + rowOffset);
+            Console.Write(message);
             rowOffset++;
-        };
+        }
+    }
+    public class Message
+    {
+        int Count;
+        string Text;
+        public Message(int count, string text)
+        {
+            Count = count;
+            Text = text;
+        }
+        public override string ToString()
+        {
+            return $"{Count}. {Text}";
+        }
     }
 }
 
+ 
