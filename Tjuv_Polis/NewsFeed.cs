dcb465dcc8b91c96a9fd361be534dedde0967457
Fredrik von Post <fredrik.v.post@gmail.com>
2024-11-04 12:11:29 +0100

    Add functionality for writing a news feed at a specific location

--- Tjuv_Polis/NewsFeed.cs
+++ Tjuv_Polis/NewsFeed.cs
@@ -4,16 +4,21 @@ namespace Tjuv_Polis;
 public class NewsFeed
 {
     public Queue<string> NewsQueue { get; }
-    private const int _maxCount = 5;
-    public NewsFeed()
+    private readonly int _maxCount;
+    private readonly int _startDrawAtX;
+    private readonly int _startDrawAtY;
+    public NewsFeed(int positionX, int positionY, int maxCount)
     {
         NewsQueue = new Queue<string>();
+        _startDrawAtX = positionX;
+        _startDrawAtY = positionY;
+        _maxCount = maxCount;
     }
     public void Add(string message)
     {
         NewsQueue.Enqueue(message);
         if (NewsQueue.Count > _maxCount)
-        { 
+        {
             NewsQueue.Dequeue();
         }
         Write();
@@ -21,9 +26,17 @@ public class NewsFeed
 
     private void Write()
     {
+        int rowOffset = 0;
         foreach (var message in NewsQueue)
         {
-            Console.WriteLine(message);
+            if (!string.IsNullOrWhiteSpace(message))
+            {
+                Console.SetCursorPosition(_startDrawAtX, _startDrawAtY + rowOffset);
+                Console.Write(new string(' ', 100));
+                Console.SetCursorPosition(_startDrawAtX, _startDrawAtY + rowOffset);
+                Console.Write(message);
+            }
+            rowOffset++;
         };
     }
 }
