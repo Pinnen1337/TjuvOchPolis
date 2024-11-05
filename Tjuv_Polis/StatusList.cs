35d7c5d292887298062532bb733f1e5f15058b91
Fredrik von Post <fredrik.v.post@gmail.com>
2024-11-05 09:38:31 +0100

    Implement StatusList and move the newsfeed

---
+++ Tjuv_Polis/StatusList.cs
@@ -0,0 +1,31 @@
+﻿
+
+namespace Tjuv_Polis
+{
+    internal class StatusList
+    {
+        private List<Person> _persons;
+        private readonly int _startDrawAtX;
+        private readonly int _startDrawAtY;
+
+        public StatusList(List<Person> persons, int positionX, int positionY)
+        {
+            _persons = persons;
+            _startDrawAtX = positionX;
+            _startDrawAtY = positionY;
+        }
+
+        internal void Write()
+        {
+            int rowOffset = 0;
+            foreach (var person in _persons)
+            {
+                Console.SetCursorPosition(_startDrawAtX, _startDrawAtY + rowOffset);
+                Console.Write(new string(' ', 50)); // Rensa raden
+                Console.SetCursorPosition(_startDrawAtX, _startDrawAtY + rowOffset);
+                Console.WriteLine(person.Status());
+                rowOffset++;
+            }
+        }
+    }
+}
